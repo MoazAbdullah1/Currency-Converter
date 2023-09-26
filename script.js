7be680e1d0309130d3c76db4223826de3395b284
MoazAbdullah1 <moazabdullah02@gmail.com>
2023-09-26 09:25:04 +0500

    initial Comment

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 function convertCurrency() {
-  
-    const amount = parseFloat(document.getElementById('amount').value);
-    const fromCurrency = document.getElementById('from').value;
-    const toCurrency = document.getElementById('to').value;
-  
-    // Replace with a real API endpoint for currency conversion
-    const apiUrl = `https://api.exchangeratesapi.io/latest?base=${fromCurrency}&symbols=${toCurrency}`;
-  
-    fetch(apiUrl)
-    
-      .then(response => response.json())
-      .then(data => {
-        const exchangeRate = data.rates[toCurrency];
-        const convertedAmount = amount * exchangeRate;
-        document.getElementById('result').innerText = `${amount} ${fromCurrency} = ${convertedAmount.toFixed(2)} ${toCurrency}`;
-      })
-      .catch(error => console.error('Error:', error));
-  }
-  
+  const amount = parseFloat(document.getElementById("amount").value);
+  const fromCurrency = document.getElementById("from").value;
+
+  const toCurrency = document.getElementById("to").value;
+
+  const apiUrl = `https://api.exchangeratesapi.io/latest?base=${fromCurrency}&symbols=${toCurrency}`;
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const exchangeRate = data.rates[toCurrency];
+      const convertedAmount = amount * exchangeRate;
+
+      document.getElementById(
+        "result"
+      ).innerText = `${amount} ${fromCurrency} = ${convertedAmount.toFixed(
+        2
+      )} ${toCurrency}`;
+    })
+
+    .catch((error) => console.error("Error:", error));
+}
